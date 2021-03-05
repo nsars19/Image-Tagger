e0e9597863b704470fdff9fa072157a3e0e6d6bb
Nick Sardonini <nsardonini@gmail.com>
2021-03-05 17:32:31 -0500

    create Form component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+const StyledForm = styled.div`
+  display: ${({ displayForm }) => (displayForm ? "flex" : "none")};
+  flex-direction: column;
+  padding: 20px;
+  background: #${({ colors }) => colors.darkBlue};
+  color: #${({ colors }) => colors.gray};
+  position: absolute;
+  top: 200px;
+  left: calc(50% - 114px);
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+  div input[type="text"] {
+    height: 35px;
+    padding: 5px;
+    border-style: none;
+    border-radius: 4px;
+    // box-shadow: 1px 1px 2px #${({ colors }) => colors.lightBlue};
+    margin: 15px 0;
+  }
+
+  div input[type="submit"] {
+    border-style: none;
+    padding: 15px 10px;
+    border-radius: 4px;
+    font-size: 14px;
+    font-weight: 600;
+
+    &:hover {
+      transition: background 0.05s ease, color 0.05s ease;
+      background: #${({ colors }) => colors.lightBlue};
+      color: #${({ colors }) => colors.cream};
+    }
+  }
+
+  & div > *:not(div) {
+    z-index: 10;
+  }
+`;
+
+function Form({ colors, toggleDisplay, displayForm, gameTime }) {
+  return (
+    <StyledForm
+      colors={colors}
+      onClick={toggleDisplay}
+      displayForm={displayForm}
+    >
+      <div>
+        <p>You finished in {gameTime}s</p>
+        <input type={"text"} placeholder={"your name here"} />
+        <input type={"submit"} value={"Submit your score!"} />
+      </div>
+    </StyledForm>
+  );
+}
+
+export default Form;
